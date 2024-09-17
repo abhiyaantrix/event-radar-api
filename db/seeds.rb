6f03b5549006f95562e5448bcd498d5443b0c314
Chetan Patil <patilchetan@live.com>
2024-09-17 21:30:14 +0200

    feat: Add database seeds
     - Create few users in different states

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Seeding data...'
+
+# running rails db:migrate:reset db:migrate:with_data db:seed together messes up the
+# column cache in models. So sometimes db:seed fails with "Unknown attribute: ...".
+# This refreshes the column cache.
+Rails.application.eager_load!
+ApplicationRecord.descendants.each(&:reset_column_information)
+
+# Users
+1.times { FactoryBot.create(:user, :archived) }
+2.times { FactoryBot.create(:user, :pending) }
+3.times { FactoryBot.create(:user) } # active
+
+puts 'Seeding data... done'
